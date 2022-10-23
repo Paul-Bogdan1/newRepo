296d4cefd11aca9932128f64d9b9701f1aea90ec
test <test@DESKTOP-T38OPS9>
2022-10-23 20:55:49 +0300

    Add project files.

---
+++ PassGen/Password.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Password.aspx.cs" Inherits="PassGen.Password" EnableViewState="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div align="center">
+            <table>
+                <caption>Password Generator</caption>
+                <thead>
+                    <tr>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>
+                            <asp:Label runat="server" ID="lblUserID" Text="UserID: "></asp:Label>
+                        </th>
+                        <td>
+                            <asp:TextBox runat="server" ID="txtUserID"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            <asp:Label runat="server" ID="lblDateTime" Text="DateTime: "></asp:Label>
+                        </th>
+                        <td>
+                            <asp:TextBox runat="server" ID="txtDateTime"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <asp:Button runat="server" ID="btnPickDate" Text="Pick Date" OnClick="btnPickDate_Click" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <asp:Button runat="server" ID="btnGeneratePassword" OnClick="btnGeneratePassword_Click" OnClientClick="F2();" Text="Generate password" />
+                            <asp:Label runat="server" ID="lblRFV" Text="Please fill in all the inputs" ForeColor="Red" Visible="false"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><asp:Label runat="server" ID="lblpsw" Text="Password: "></asp:Label></th>
+                        <td>
+                            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+                            <asp:Timer ID="Timer1" runat="server" OnTick="Timer1_Tick" Interval="1000">
+                            </asp:Timer>
+                            <asp:UpdatePanel ID="UpdatePanel1" UpdateMode="Conditional" runat="server">
+                                <ContentTemplate>
+                                    <asp:Label runat="server" ID="lblPassword"></asp:Label>
+                                    <br />
+                                    <asp:Literal ID="litmsg" runat="server"></asp:Literal>
+                                </ContentTemplate>
+                                <Triggers>
+                                    <asp:AsyncPostBackTrigger ControlID="Timer1" EventName="Tick" />
+                                </Triggers>
+                            </asp:UpdatePanel>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PassGen/Password.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Timers;
+
+namespace PassGen
+{
+    public partial class Password : System.Web.UI.Page
+    {
+        string userID = "";
+        string datePicked = "";
+        string letterDate = "";
+        string finalPassword = "";
+        Random r;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Timer1.Enabled = false;
+            }
+        }
+
+        protected void btnPickDate_Click(object sender, EventArgs e)
+        {
+            txtDateTime.Text = Calendar1.SelectedDate.ToString();
+        }
+
+        protected void btnGeneratePassword_Click(object sender, EventArgs e)
+        {
+            lblPassword.Visible = true;
+            if (txtUserID.Text == "" || txtDateTime.Text == "")
+            {
+                lblRFV.Visible = true;
+            }
+            else
+            {
+                lblRFV.Visible = false;
+                userID = Convert.ToString(txtUserID.Text);
+                datePicked = Convert.ToString(txtDateTime.Text);
+
+                for (int i = 0; i < datePicked.Length; i++)
+                {
+                    string letter = datePicked.Substring(i, 1);
+                    if (letter != "/" && letter != " " && letter != ":")
+                    {
+                        letterDate += letter;
+                    }
+                }
+
+                int userIDSize = userID.Length;
+                int letterDateSize = letterDate.Length;
+
+                if (userIDSize <= letterDateSize)
+                {
+                    for (int i = 0; i < letterDateSize; i++)
+                    {
+                        userID += letterDate.Substring(i, 1);
+                    }
+                    string[] userArr = new string[userID.Length];
+                    for (int i = 0; i < userID.Length; i++)
+                    {
+                        userArr[i] = userID.Substring(i, 1);
+                    }
+                    CreatePassword(userArr, userID.Length);
+                }
+                else if (userIDSize >= letterDateSize)
+                {
+                    for (int i = 0; i < userIDSize; i++)
+                    {
+                        letterDate += userID.Substring(i, 1);
+                    }
+                    string[] letterArr = new string[letterDate.Length];
+                    for (int i = 0; i < letterDate.Length; i++)
+                    {
+                        letterArr[i] = letterDate.Substring(i, 1);
+                    }
+                    CreatePassword(letterArr, letterDate.Length);
+                }
+            }
+        }
+
+        protected void CreatePassword(string[] pass, int size)
+        {
+            r = new Random();
+            for (int i = 0; i < pass.Length; i++)
+            {
+                finalPassword += pass[r.Next(0, size)];
+            }
+
+            lblPassword.Text = finalPassword;
+            Timer1.Enabled = true;
+            Session["Timer"] = DateTime.Now.AddSeconds(32).ToString();
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (DateTime.Compare(DateTime.Now, DateTime.Parse(Session["Timer"].ToString())) < 0)
+            {
+                litmsg.Text = "Time left: " + (((Int32)DateTime.Parse(Session["Timer"].ToString()).Subtract(DateTime.Now).TotalSeconds) % 60).ToString() + " Seconds";
+            }
+            else
+            {
+                litmsg.Text = "Password has expired, please generate a new one!";
+                lblPassword.Visible = false;
+            }
+
+        }
+    }
+}
